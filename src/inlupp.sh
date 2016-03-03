5f688e64f19eb93eeb1953e00726bb33f12fd372
Jonas Sjöberg <jomeganas@gmail.com>
2016-03-03 22:43:16 +0100

    Update ..

--- src/inlupp.sh
+++ src/inlupp.sh
@@ -17,16 +17,44 @@
 # Den här filens namn, utan fullständig sökväg.
 SCRIPT_NAME=$(basename $0)
 
+# Hämta färginformation, spara till variabler.
+C_NORMAL=$(tput sgr0)
+C_RED=$(tput setaf 1)
+C_GREEN=$(tput setaf 2)
+C_YELLOW=$(tput setaf 3)
+
+
+function msg()
+{
+    TAB='  '
+    MSG_FORMAT="%s\n"
+
+    printf "${GREEN}[${SCRIPT_NAME}]${NORMAL}${TAB}"
+    printf "$MSG_FORMAT" "$@"
+}
 
 function msg_error()
 {
-    FORMAT="%s
-    printf
+    TAB='  '
+    MSG_FORMAT="%s\n"
+
+    printf "${RED}[${SCRIPT_NAME}]${NORMAL}${TAB}"
+    printf "$MSG_FORMAT" "$@"
 }
 
-function create_fil_ett()
+function msg_warning()
+{
+    TAB='  '
+    MSG_FORMAT="%s\n"
+
+    printf "${YELLOW}[${SCRIPT_NAME}]${NORMAL}${TAB}"
+    printf "$MSG_FORMAT" "$@"
+}
+
+function create_file()
 {
     local dest="$1"
+    local cont="$2"
 
     if [ -z "$dest_path" ]
     then
@@ -43,7 +71,7 @@ function create_fil_ett()
             return 1
         else
             # Skapar en fil med innehållet 'fil ett'.  
-            echo 'fil ett' > "$dest"
+            echo "$cont" > "$dest"
             # Använd returvärdet från echo-operationen som returvärde för
             # funktionen 'create_fil_ett'.
             return $*
@@ -51,7 +79,63 @@ function create_fil_ett()
     fi
 }
 
+function create_folder()
+{
+    local dest="$1"
 
+    mkdir -vp "$1"
+}
+
+
+d1="laborationett"
+d2="${d1}/katalogen"
+d3="${d1}/katalogto"
+cat 'fil ett' > "filett.txt"
+
+mkdir -v "$d1"
+cat 'fil två' > "${d1}/filtvaa.txt"
+cat 'fil tre' > "${d1}/filtreetxt"
+
+mkdir -v "${d2}"
+touch "${d2}/skalpgm.sh" 
+
+touch "${d2}/data.txt"        innehåll: godtycklig text, hitta på något.
+cat <<EOF
+BEOWULF.
+
+
+
+
+I.
+
+THE LIFE AND DEATH OF SCYLD.
+
+
+{The famous race of Spear-Danes.}
+
+          Lo! the Spear-Danes' glory through splendid achievements
+          The folk-kings' former fame we have heard of,
+          How princes displayed then their prowess-in-battle.
+
+{Scyld, their mighty king, in honor of whom they are often called
+Scyldings. He is the great-grandfather of Hrothgar, so prominent in the
+poem.}
+
+          Oft Scyld the Scefing from scathers in numbers
+        5 From many a people their mead-benches tore.
+          Since first he found him friendless and wretched,
+          The earl had had terror: comfort he got for it,
+          Waxed 'neath the welkin, world-honor gained,
+          Till all his neighbors o'er sea were compelled to
+       10 Bow to his bidding and bring him their tribute:
+          An excellent atheling! After was borne him
+
+{A son is born to him, who receives the name of Beowulf--a name afterwards
+made so famous by the hero of the poem.}
+EOF
+
+laborationett/katalogto/data.txt        innehåll: godtycklig text, något annat.
+laborationett/katalogto/filfyra.txt     innehåll: ”fil fyra”.
 
 
 
